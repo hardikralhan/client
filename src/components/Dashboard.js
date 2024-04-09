e834d6ac98693aed1fd0f77f54bf5e7be902f23b
hardikralhan66@gmail.com <hardikralhan66@gmail.com>
2024-04-09 18:32:06 +0530

    add recipe button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -102,6 +102,11 @@ export default function Dashboard() {
                 Get Recipe</button>
                 </div>
 
+                <div className='ml-24 mr-40 '>
+                <button onClick={()=>{navigate("/add-recipe")}} className="flex items-center justify-center px-8 h-14 text-md font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                Add a Recipe</button>
+                </div>
+
                 <div className="flex w-full px-3 mb-12">
                         <span className="text- font-semibold px-1"></span>
                         <select
